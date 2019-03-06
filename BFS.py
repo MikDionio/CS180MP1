7cbbd1b54d94f28bb4f9ebf44ff5a8fa37cf3595
Mikhael Dionio <mikhaeldionio@gmail.com>
2019-03-06 16:27:12 +0800

    implement BFS

--- BFS.py
+++ BFS.py
@@ -20,14 +20,95 @@ def goalPosition():
 def initMap(start, goal):
     map = [[] for i in range(101)]
 
-    for i in range(100):
-        for j in range(200):
-            map[i].append(math.sqrt((goal[1]-i)**2 + (goal[0]-j)**2))
+    #This is heuristic function for A*
+    # for i in range(100):
+    #     for j in range(200):
+    #         map[i].append(math.sqrt((goal[1]-i)**2 + (goal[0]-j)**2))
 
-    print(map[0][5])
-    return
+    #List all values for map as 0, meaning they have not been visited yet
+    for i in range (100):
+        for j in range(200):
+            map[i].append(0)
+
+    return map
+
+def remove(list):
+    n = list[0]
+    list.pop(0)
+    return n
+
+def BFS(S, G, map, fringe):
+
+    if(G == S):
+        map[S[0]][S[1]] = S
+    else:
+        while(1):
+            if not fringe:
+                break
+
+            n = fringe.pop(0)
+            #print("N: " + str(n))
+            #print(fringe)
+            
+            #Check if dequeued node is G
+            if(n == G):
+                break
+            #Expand dequeued node
+            down = n[1] + 1
+            up = n[1] - 1
+            left = n[0] - 1
+            right = n[0] + 1
+
+            #Insert unvisisted nodes to fringe
+            if(left >= 0):
+                if(map[left][n[1]] == 0):
+                    fringe.append((left,n[1]))
+                    map[left][n[1]] = n
+            if(right < 100):
+                if(map[right][n[1]] == 0):
+                    fringe.append((right,n[1]))
+                    map[right][n[1]] = n
+            if(up >= 0):
+                if(map[n[0]][up] == 0):
+                    fringe.append((n[0],up))
+                    map[n[0]][up] = n
+            if(down < 200):
+                if(map[n[0]][down] == 0):
+                    fringe.append((n[0],down))
+                    map[n[0]][down] = n
+
+            #DFS
+            # if(left >= 0 and map[left][n[1]] == 0):
+            #     fringe.insert(0,map[left][n[1]])
+            # if(right<= 100 and map[right][n[1]] == 0):
+            #     fringe.insert(0,map[left][n[1]])
+            # if(up >= 0 and map[n[0]][up] == 0):
+            #     fringe.insert(0,map[n[0]][up])
+            # if(down <= 200 and map[n[0]][down] == 0):
+            #     fringe.insert(0,map[n[0]][down])
+
+    return map
+
+def getPath(S,G,map):
+    path = []
+    path.append(G)
+    p = G
+    while(path[0] != S):
+        p = (map[p[0]][p[1]])
+        path.insert(0,p)
+
+    return path
 
 #Main
 
-#Initialize Map
-initMap(startPosition(), goalPosition())
+#Input
+S = startPosition()
+G = goalPosition()
+
+#Initialize map
+map = initMap(S, G)
+
+fringe = [S]
+map[S[0]][S[1]] = S #Denote starting node as visited
+BFS(S,G,map,fringe)
+print(getPath(S,G,map))
